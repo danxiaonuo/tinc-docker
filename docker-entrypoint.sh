a1ffc8c782544021d6a86812c2382c80eaca14be
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-01-11 19:17:41 +0800

    Create docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+
+/init.sh
+
+if ! [[ -c /dev/net/${NETWORK} ]]
+then
+    mkdir -p /dev/net
+    mknod /dev/net/${NETWORK} c 10 200
+fi
+
+if [[ $RUNMODE != server ]]
+then
+   sed -i -e '/公网IP地址/d' -e '/Address/d' /etc/tinc/${NETWORK}/hosts/${NODE} 
+fi
+
+exec tincd --no-detach \
+           --net=${NETNAME} \
+           --debug=${VERBOSE} \
+           "$@"
