18957dd6ceeed01b5f8259c51c205376f87ce86e
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-01-12 03:12:57 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,8 @@
 
 /init.sh
 
-if ! [[ -c /dev/net/${NETWORK} ]]
-then
-    mkdir -p /dev/net
-    mknod /dev/net/${NETWORK} c 10 200
-fi
-
-if [[ $NODE != server ]]
-then
-   sed -i -e '/公网IP地址/d' -e '/Address/d' /etc/tinc/${NETWORK}/hosts/${NODE} 
-fi
-
-exec tincd --no-detach \
-           --net=${NETNAME} \
-           --debug=${VERBOSE} \
+exec tinc --net=${NETNAME} start \
+          --no-detach \
+          --debug=${VERBOSE} \
+		  --logfile=/var/log/tinc.${NETNAME}.log \
            "$@"
