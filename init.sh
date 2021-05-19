56c0602272195acf4956243803891303414e5031
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-05-19 09:43:47 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -142,9 +142,6 @@ for host in $peers; do
 done
 
 
-# 生成密钥
-yes "" | tinc -n ${NODE} generate-keys >/dev/null 2>&1
-
 # 设置hosts文件
 cat >>/etc/tinc/${NETNAME}/hosts/${NODE} <<_EOF_
 #定义tinc内网网段
@@ -178,6 +175,9 @@ _EOF_
 # 设置文件权限
 chmod +x /etc/tinc/"${NETNAME}"/tinc-up
 chmod +x /etc/tinc/"${NETNAME}"/tinc-down
+
+# 生成密钥
+yes "" | tinc -n ${NODE} generate-keys >/dev/null 2>&1
 
 	fi
 
