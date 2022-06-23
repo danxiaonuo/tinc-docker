b5335f6b93b0d8be898a60ff1a5f8e495252732e
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-06-23 22:34:53 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -312,6 +312,12 @@ ip -6 link set ${INTERFACE} up mtu 1500
 ip addr add ${PRIVATE_IPV4}/${PRIVATE_IPV4_MASK} dev ${INTERFACE}
 ip -6 addr add ${PRIVATE_IPV6}/${PRIVATE_IPV6_MASK} dev ${INTERFACE}
 ip -6 route add ${PRIVATE_IPV6_GW}/${PRIVATE_IPV6_MASK} dev ${INTERFACE} metric 1
+iptables -t nat -A POSTROUTING -s "${PRIVATE_IPV4_GW}/${PRIVATE_IPV4_MASK}" ! -o xiaonuo -j MASQUERADE
+ip6tables -t nat -A POSTROUTING -s "${PRIVATE_IPV6_GW}/${PRIVATE_IPV6_MASK}" ! -o xiaonuo -j MASQUERADE
+iptables -A FORWARD -i xiaonuo -j ACCEPT
+ip6tables -A FORWARD -i xiaonuo -j ACCEPT
+iptables -A FORWARD -o xiaonuo -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -A FORWARD -o xiaonuo -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 _EOF_
 
 cat >/etc/tinc/${NETNAME}/tinc-down <<_EOF_
@@ -320,6 +326,12 @@ ip route del ${PRIVATE_IPV4_GW}/${PRIVATE_IPV4_MASK} dev ${INTERFACE}
 ip -6 route del ${PRIVATE_IPV6_GW}/${PRIVATE_IPV6_MASK} dev ${INTERFACE}
 ip link set ${INTERFACE} down
 ip -6 link set ${INTERFACE} dow
+iptables -t nat -D POSTROUTING -s "${PRIVATE_IPV4_GW}/${PRIVATE_IPV4_MASK}" ! -o xiaonuo -j MASQUERADE
+ip6tables -t nat -D POSTROUTING -s "${PRIVATE_IPV6_GW}/${PRIVATE_IPV6_MASK}" ! -o xiaonuo -j MASQUERADE
+iptables -D FORWARD -i xiaonuo -j ACCEPT
+ip6tables -D FORWARD -i xiaonuo -j ACCEPT
+iptables -D FORWARD -o xiaonuo -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+ip6tables -D FORWARD -o xiaonuo -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 _EOF_
 
 # 设置文件权限
